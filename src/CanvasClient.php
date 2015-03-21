fb654dace63f777e5b92dd3297784720a00c14cf
mikerowe81 <mikerowe81@gmail.com>
2015-03-20 22:19:19 -0400

    Initial Commit

---
+++ src/CanvasClient.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Ecampusweb\CanvasLmsPhpApi;
+
+
+class CanvasClient
+{
+
+    private $options = [
+        'base_url' => '',
+        'token'    => '',
+        'per_page' => '50',
+    ];
+
+    private $getRequests = [
+        'listAccounts' => [
+            'path' => '/api/v1/accounts'
+        ],
+        'getAccount'   => [
+            'path' => '/api/v1/accounts/:id'
+        ],
+    ];
+
+
+    public function __call($name, $arguments)
+    {
+        preg_replace_callback(
+            '/:(\w+)/',
+            function ($matches) use ($arguments) {
+                if(!isset($arguments[$matches[1]])) {
+                    throw new \Exception("Required argument not set: ".$matches[1]);
+                }
+
+                return $arguments[$matches[1]];
+            },
+            $this->getRequests[$name]['path']
+        );
+    }
+
+    public function init(array $options)
+    {
+        $this->options = array_merge($this->options, $options);
+    }
+
+    public function listAccounts()
+    {
+        return [
+            'method' => 'GET',
+            'url'    => '/api/v1/accounts',
+        ];
+    }
+}
